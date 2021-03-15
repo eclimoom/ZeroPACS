6a41a14d2606ad879a8e6d97b7319f5c2c8b7558
moom <junjihe@live.com>
2021-03-15 23:35:54 +0800

    feature: 1. init viewer front-end

---
+++ frontend/viewer/src/app/pages/home/components/study-list/study-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-study-list',
+  templateUrl: './study-list.component.html',
+  styleUrls: ['./study-list.component.scss']
+})
+export class StudyListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/viewer/src/app/pages/study-detail/study-detail/study-detail.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StudyDetailComponent } from './study-detail.component';
+
+describe('StudyDetailComponent', () => {
+  let component: StudyDetailComponent;
+  let fixture: ComponentFixture<StudyDetailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ StudyDetailComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StudyDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
